7ff723773cd8be69b06386e51b46fbc9602d8e6d
JJowoo <34655168+JJowoo@users.noreply.github.com>
2022-09-06 21:45:02 +0900

    commit test

--- app/src/main/java/com/example/myapplication/login.kt
+++ app/src/main/java/com/example/myapplication/login.kt
@@ -14,10 +14,12 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //git test code
         /*
         val button_login=findViewById<Button>(R.id.btn_login)
         val et_email= findViewById<EditText>(R.id.editText_email)
         val et_password =findViewById<EditText>(R.id.editText_Password)
+
 
 
         */
